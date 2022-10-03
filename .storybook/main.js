f0e9954f7f8a808a3d4316f8a7555d0c7b592722
Fernandotmk <fernando.tmk@hotmail.com>
2022-10-03 12:17:06 -0300

    Building storybook-static

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
+  addons: ['@storybook/addon-essentials'],
   "stories": [
     "../src/components/**/stories.mdx",
     "../src/components/**/stories.@(js|jsx|ts|tsx)"
@@ -8,8 +9,9 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
   ],
+  framework: '@storybook/react',
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
-  }
+  },
 }
